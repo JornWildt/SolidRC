833863154163b79b7861b30734ded0de8f93c266
JornWildt <jw@elfisk.dk>
2018-11-23 13:45:46 +0100

    Add login window. Not used yet.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,11 @@ namespace SolidRC.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
